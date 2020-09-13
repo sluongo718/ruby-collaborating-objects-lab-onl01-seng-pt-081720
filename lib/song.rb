c9c88cf841d79021da25afbff64a806f6e0346f7
sam l <sluongo718@yahoo.com>
2020-09-13 20:43:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,8 +15,11 @@ class Song
     @@all
   end 
   
-  def self.new_by_filename
-    
+  def self.new_by_filename(filename)
+    split_name = filename.split(" - ")
+    song = self.new(split_name[1])
+    artist = Artist.find_or_create_by_name(split_name[0])
+    song.artist = artist
   end
   
   
