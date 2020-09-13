04ebb92e532e19175ee5481fa3e44829d9c81d69
sam l <sluongo718@yahoo.com>
2020-09-13 20:50:39 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -24,7 +24,7 @@ class MP3Importer
   def import 
     self.files.each do |file|
       song = Song.new_by_filename(file)
-      Artist.all << song.artist unless artist.all.include?(song.artist)
+      Artist.all << song.artist unless Artist.all.include?(song.artist)
       
     end
   end
